8c5f0b48cb7d387fabf1bd0b5cbee919cc832b0c
Brosimov <egorabrosimov5g@yandex.ru>
2024-02-15 11:23:24 +0500

    Added logs in Logcat

--- app/src/main/java/BaseActivity.kt
+++ app/src/main/java/BaseActivity.kt
@@ -10,31 +10,37 @@ open class BaseActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(javaClass.simpleName, "OnCreate")
         writeToFile("onCreate")
     }
 
     override fun onStart() {
         super.onStart()
+        Log.d(javaClass.simpleName, "onStart")
         writeToFile("onStart")
     }
 
     override fun onResume() {
         super.onResume()
+        Log.d(javaClass.simpleName, "onResume")
         writeToFile("onResume")
     }
 
     override fun onPause() {
         super.onPause()
+        Log.d(javaClass.simpleName, "onPause")
         writeToFile("onPause")
     }
 
     override fun onStop() {
         super.onStop()
+        Log.d(javaClass.simpleName, "onStop")
         writeToFile("onStop")
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        Log.d(javaClass.simpleName, "onDestroy")
         writeToFile("onDestroy")
     }
 
